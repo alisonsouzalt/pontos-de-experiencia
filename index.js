08e4dde0bfbd3e7a3371d1a7046f04da90c6837c
alisonsouzalt <alisonsouzalt@hotmail.com>
2023-12-17 16:10:38 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+//Desafios JavaScript na DIO têm funções "gets" e "print" acessíveis globalmente:
+//- "gets" : lê UMA linha com dado(s) de entrada (inputs) do usuário;
+//- "print": imprime um texto de saída (output), pulando linha.
+
+// Entrada de dados. Lembre-se: O parseInt(()) é importante para a conversão dos valores de entrada(String) para um valor numérico(int).
+let num1 = parseInt(gets());
+let num2 = parseInt(gets());
+
+//TODO: Implemente a lógica para exibir o xpGanho:
+const xpGanho = heroX();
+
+function heroX(){
+  let constant = 100
+  let xp = num1 * num2 * constant
+  return xp
+  
+}
+
+// Imprime a quantidade de XP ganho
+print("Voce ganhou " + xpGanho + " XP!");
